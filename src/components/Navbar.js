ef1a8da52b56cae22e6cfbca3c3380728b0c4e11
franxifra <franxifra@users.noreply.github.com>
2021-06-15 17:08:29 -0300

    ultima correccion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,28 +12,24 @@ const Navbar = () => {
 
         <Menu.Item
           name='mochilas'
-          onClick={this.handleItemClick}
         >
           <a href="">Mochilas</a>
         </Menu.Item>
 
         <Menu.Item
           name='carpas'
-          onClick={this.handleItemClick}
         >
           <a href="">Carpas</a>
         </Menu.Item>
 
         <Menu.Item
           name='accesorios'
-          onClick={this.handleItemClick}
         >
           <a href="">Accesorios</a>
         </Menu.Item>
         <Menu.Menu position='right'>
           <Menu.Item
             name='cartWidget'
-            onClick={this.handleItemClick}
           >
             <CartWidget/>
           </Menu.Item>
